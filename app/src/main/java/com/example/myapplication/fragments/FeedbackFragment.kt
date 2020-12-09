83ea912162a0de76e5b9e44f0732749ddc5ae43e
LlevaniGit <levani.chkhutiashvili.1@gmail.com>
2020-12-09 20:14:36 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/fragments/FeedbackFragment.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class FeedbackFragment {
+    class FeedbackFragment : Fragment(R.layout.fragment_feedback) {
+    }
+}
